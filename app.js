e24ffa48a85d93bc951a1c95f6d59114c961fc81
yezizheng <yezi@gwmail.gwu.edu>
2018-03-27 00:00:10 -0700

    rm node_modules

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v9", { useMongoClient: true });
+mongoose.connect("mongodb://localhost/yelp_camp_v5", { useMongoClient: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
